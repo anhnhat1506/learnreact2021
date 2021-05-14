252f2a97f55336f067335656d7999476af02b20f
anhnhat1506 <nhat15061992@gmail.com>
2021-05-14 17:59:17 +0700

    component

---
+++ react-app/src/components/Todos.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react'
+
+const Todos = () => {
+    return (
+        <Fragment>
+            <p>Việc 1</p>
+            <p>Việc 2</p>
+            <p>Việc 3</p>
+        </Fragment>
+        
+    )
+}
+
+export default Todos
